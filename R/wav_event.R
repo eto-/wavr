6ccf7fa9b4ec25c3a56da74cee001513b68c522a
Alessandro Razeto <razeto@lngs.infn.it>
2022-10-09 14:34:27 +0200

    Initial commit

---
+++ R/wav_event.R
@@ -0,0 +1,132 @@
+wav_keep_version_0 <- F
+
+wav_event <- function (filename, offset, data=T) {
+  header.size <- 20
+  to_32 <- function (v) v[1] + 65536 * v[2]
+
+  f <- cpp_read_sndfile (filename, from=offset, to=offset + header.size - 1)
+  if (0xFFFF != f$data[1]) stop("marker not found")
+  if (header.size != f$data[2]) stop("wrong header size")
+  counter <- to_32 (f$data[3:4])
+  time.tag <- to_32 (f$data[5:6])
+  n.samples <- to_32 (f$data[7:8])
+  cpu.time.ms <- to_32 (f$data[9:10])
+  n.channels <- f$data[11]
+  version <- f$data[12] / 100
+
+  # keep lagacy data
+  if (wav_keep_version_0 && !is.null(f$date) && as.Date(f$date, "%c") < as.Date("2016-08-24")) {
+    n.channels <- 1
+    version <- 0
+  } else if (version == 0) version = 1
+
+  data.size <- if (version >= 2) to_32 (f$data[13:14]) else n.channels * n.samples
+  
+  v <- list(counter=counter, time.tag=time.tag, n.samples=n.samples, n.channels=n.channels, cpu.time.ms=cpu.time.ms, version=version, 
+	    offset=offset, header.size=header.size, data.size=data.size, sample.rate=f$sample_rate, sample.nbits=f$sample_nbits, date=f$date, raw.data=f$data)
+
+
+  if (data) {
+    f <- cpp_read_sndfile (filename, from=offset + header.size, to=offset + header.size + data.size - 1, metadata=F)
+    if (n.channels == 1) {
+      if (version < 2) v$data <- list('1' = f$data)
+      else {
+	v$data <- list(tail(f$data, -4))
+	names(v$data) <- as.character(f$data[1])
+      }
+    } else {
+      if (version < 2) v$data <- split(f$data, rep(1:n.channels, each=n.samples))
+      else {
+	v$data <- vector("list", length = n.channels)
+	idx <- 1
+	for (i in seq(n.channels)) {
+	  id <- f$data[idx]
+	  n <- f$data[idx + 1]
+	  v$data[[i]] <- f$data[seq(idx + 5, idx + n)]
+	  names(v$data)[i] <- as.character(id)
+	  idx <- idx + 4 + n
+	}
+      }
+    }
+  }
+  v
+}
+
+wav_entries <- function (filename) {
+  if(!file.exists(filename)) stop(paste("file", filename, "not present"))
+
+  f <- cpp_read_sndfile (filename, to=0)
+  total.size <- f$samples
+  index <- rep(0, 10000)
+  offset <- 1
+  n <- 0
+  while (offset < total.size) {
+    e <- wav_event(filename, offset, data=F)
+    n <- n + 1
+    if (n >= length(index)) index <- c(index, rep(0, n * 10))
+    index[n] <- offset
+    offset <- offset + e$header.size + e$data.size
+  }
+  attr(n, "index") <- index[1:n]
+  n
+}
+
+wav_apply <- function (filename, index=NULL, start=1, entries=Inf, data=T, apply_fun=sapply, fun, ...) {
+  indexes <- attr(wav_apply, "indexes")
+  if (is.null(indexes)) indexes <- list()
+  if (is.null(index)) index <- indexes[[filename]]
+
+  if (is.null(index)) {
+    cat("Building index\n")
+    t <- wav_entries (filename)
+    index <- attr(t, "index")
+  } 
+  total_entries <- length(index)
+
+
+  end <- ifelse(is.infinite(entries), total_entries, ifelse(entries > 0, min(start + entries - 1, total_entries), total_entries + entries))
+		
+  if (end < start) return()
+  cat("Processing", end - start + 1, "entries from", filename, "\n")
+
+  process_event <- function (id, indexes, filename, data, fun, ...) fun(wav_event(filename, indexes[id], data), id=id,...)
+
+  k <- apply_fun(start:end, process_event, index, filename, data, fun, ...)
+
+
+  indexes[[filename]] <- index
+  attr(wav_apply, "indexes") <<- indexes
+
+  attr(k, "index") <- index
+  k
+}
+
+unroll_timetag <- function (time.tag, cpu.time, tag.step=8e-9) {
+  d <- data.frame(tag=time.tag, cpu=cpu.time)
+  d$rt <- diff(c(0, d$cpu))
+  d$dt <- diff(c(0, d$tag))
+  d$dt[d$dt < 0] <- 2**31 + d$dt[d$dt < 0]
+  d$dt <- d$dt * tag.step
+  d$dt[d$rt > 5] <- d$rt[d$rt > 5]
+  t <- cumsum(d$dt)
+  t
+}
+
+analyze <- function (file, fun, entries=0, parallel=T, ...) {
+  f_ <- function (x, id, ...)  {
+    h <- c(id=id, time=x$time.tag, cpu.time=x$cpu.time.ms * 1e-3)
+    r <- fun(x, ...)
+    c(h, r)
+  }
+  
+  v <- wav_apply(file, entries=entries, data=T, apply_fun=ifelse(parallel,  mclapply, lapply), fun=f_, ...)
+  if (exists("rbindlist")) d <- rbindlist(v) #, fill=T)
+  else d <- do.call(rbind.data.frame, v)
+
+  d$time <- unroll_timetag(d$time, d$cpu.time)
+  d$name <- file
+  d$index <- attr(v, "index")[d$id]
+  d$unique <- diff(c(0, d$id)) > 0
+
+  d
+}
---
+++ src/read_wav.cpp
@@ -0,0 +1,44 @@
+#include <Rcpp.h>
+#include <sys/stat.h>
+#include <sndfile.hh>
+#include <vector>
+#include <iostream>
+
+Rcpp::NumericVector read_n (SndfileHandle& file, int len) {
+  std::vector<short int> v(len);
+  sf_count_t c = file.read (&v[0], len);
+
+  if (c != len) {
+    std::cerr << "error: " << file.strError() << std::endl;
+    return Rcpp::NumericVector(0);
+  }
+
+  Rcpp::NumericVector left(len);
+  for (int i = 0; i < len; i++) left[i] = (uint16_t)v[i];
+
+  return left;
+}
+
+// [[Rcpp::export]]
+Rcpp::List cpp_read_sndfile (const char * fname, int from=1, int to=2147483645, bool metadata=true) {
+  struct stat buffer;   
+  if (::stat (fname, &buffer)) Rcpp::stop("missing file %s", fname);
+
+  SndfileHandle file = SndfileHandle (fname) ;
+  Rcpp::List r;
+
+  if (metadata) {
+    r["samples"] = file.frames ();
+    r["sample_rate"] = file.samplerate ();
+    r["bits"] = 16;
+    r["date"] = file.getString (SF_STR_DATE);
+  }
+
+  if (to <= 0 || to >= from) {
+    file.seek (from - 1, SEEK_SET);
+    r["data"] = read_n(file, to - from + 1);
+  } 
+
+
+  return(r);
+}
